c254e930d8a3cdf8e1124131d848c81c49d2b46c
Pavel <89654104202@mail.ru>
2021-09-23 18:14:38 +0300

    Fix

--- src/Main14.java
+++ src/Main14.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 public class Main14 {
     public static void main(String[] args) {
         int[] arr = new int[15];
-        int max = arr[0];
-        int min = arr[0];
+        int max = 0;
+        int min = 0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 40) - 20;
+            max = arr[i];
+            min = arr[i];
         }
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
+
             if (arr[i] > max) {
                 max = arr[i];
             }
-        }
-        for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) {
                 min = arr[i];
             }
