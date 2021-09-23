5b21c302028582992faf032fec8b3022b3058174
Pavel <89654104202@mail.ru>
2021-09-23 18:17:33 +0300

    Fix

--- src/Main14.java
+++ src/Main14.java
@@ -9,6 +9,7 @@ public class Main14 {
             arr[i] = (int) (Math.random() * 40) - 20;
             max = arr[i];
             min = arr[i];
+
         }
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
